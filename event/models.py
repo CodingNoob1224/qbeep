be9a86609fc05ba488ddfbd3a9d6e9e59031bb5c
PeiYunKuo <CodingNoob1224@gmail.com>
2024-10-05 15:03:58 +0800

    1005_PeiYun

--- event/models.py
+++ event/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from members.models import User  # Import the User model from the member app
 
 # Create your models here.
+class Event(models.Model):
+    title=models.CharField(max_length=20)
+    description=models.TextField
+    time=models.DateTimeField()
+    quota=models.IntegerField()
+    last_updated=models.DateTimeField(auto_now=True)
+    amount=models.IntegerField(default=0)
+    
+class signup(models.Model):
+    event=models.OneToOneField(Event,on_delete=models.CASCADE)
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    signup_time=models.DateTimeField(auto_now_add=True)
+    checkin=models.BooleanField(default=False)
+    checkin_time=models.DateTimeField(Null=True)
+    checkout=models.BooleanField(default=False)
+    checkout_time=models.DateTimeField(Null=True)
+    
--- members/models.py
+++ members/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from event.models import Event 
 # Create your models here.
+class User(models.Model):
+    name=models.CharField(max_length=10)
+    email=models.EmailField(unique=True)
+    phone=models.CharField(max_length=10)
+    GENDER_CHOICES=[
+        ("F","Female"),
+        ("M","Male")
+    ]
+    gender=models.CharField(max_length=1, choices=GENDER_CHOICES)
+    birth_date=models.DateField()
